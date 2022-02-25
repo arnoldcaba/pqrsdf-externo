5420c6a0346dd4931ff33b8749b96b7f7b9e576a
Arnold Caballero <arnoldcaba@gmail.com>
2022-02-24 21:34:55 -0500

    first deploy

---
+++ precache-manifest.9aa9123b34afa0efc96f82b0040def28.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a93169facada463a406",
+    "url": "/css/Autenticacion.ade3e926.css"
+  },
+  {
+    "revision": "b135217119190472c275",
+    "url": "/css/Autenticacion~RegistroBasico.fae4c610.css"
+  },
+  {
+    "revision": "c7f728e7d8b9a031c012",
+    "url": "/css/RegistroBasico.a1b89edc.css"
+  },
+  {
+    "revision": "57519dce1f5bddbced2b",
+    "url": "/css/TableRegister.4f03704a.css"
+  },
+  {
+    "revision": "6243ef1d094d9890c8fd",
+    "url": "/css/TableRegister~recovery~registro.41762425.css"
+  },
+  {
+    "revision": "b3c95c4ff903347e8898",
+    "url": "/css/TableRegister~registro.90943f80.css"
+  },
+  {
+    "revision": "158b309cb229ac73463d",
+    "url": "/css/app.721dcd64.css"
+  },
+  {
+    "revision": "6c85036e7c3baeae368d",
+    "url": "/css/chunk-vendors.c439af37.css"
+  },
+  {
+    "revision": "c2ecdcc53513ad8013ae",
+    "url": "/css/registro.9b996351.css"
+  },
+  {
+    "revision": "128814e08d97206dcdc084bca7aeaf8c",
+    "url": "/img/logo-alcaldia-piedecuesta.128814e0.png"
+  },
+  {
+    "revision": "9cb18b6f8788786e1889668558934260",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a93169facada463a406",
+    "url": "/js/Autenticacion.e4bac093.js"
+  },
+  {
+    "revision": "b135217119190472c275",
+    "url": "/js/Autenticacion~RegistroBasico.66f102d0.js"
+  },
+  {
+    "revision": "c7f728e7d8b9a031c012",
+    "url": "/js/RegistroBasico.21bb1b16.js"
+  },
+  {
+    "revision": "57519dce1f5bddbced2b",
+    "url": "/js/TableRegister.c545d7ad.js"
+  },
+  {
+    "revision": "6243ef1d094d9890c8fd",
+    "url": "/js/TableRegister~recovery~registro.46431029.js"
+  },
+  {
+    "revision": "b3c95c4ff903347e8898",
+    "url": "/js/TableRegister~registro.98acc4a6.js"
+  },
+  {
+    "revision": "158b309cb229ac73463d",
+    "url": "/js/app.0b308881.js"
+  },
+  {
+    "revision": "6c85036e7c3baeae368d",
+    "url": "/js/chunk-vendors.ef5d6c41.js"
+  },
+  {
+    "revision": "bd685c6db31fa080cc4e",
+    "url": "/js/recovery.72eb5e5a.js"
+  },
+  {
+    "revision": "c2ecdcc53513ad8013ae",
+    "url": "/js/registro.8d28f161.js"
+  },
+  {
+    "revision": "cb9f393fed3c4fcc07bcc97518819636",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
